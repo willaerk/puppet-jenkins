20bbd0c7d91d0ac0c1162ebe16018de0424ceb9a
Kristof Willaert <kristof.willaert@cultuurnet.be>
2016-02-10 16:52:40 +0100

    Update apt repo syntax to new version

--- manifests/repo/debian.pp
+++ manifests/repo/debian.pp
@@ -14,9 +14,14 @@ class jenkins::repo::debian
       location    => 'http://pkg.jenkins-ci.org/debian-stable',
       release     => 'binary/',
       repos       => '',
-      key         => '150FDE3F7787E7D11EF4E12A9B7D32F2D50582E6',
-      key_source  => 'http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key',
-      include_src => false,
+      key         => {
+        id      => '150FDE3F7787E7D11EF4E12A9B7D32F2D50582E6',
+        source  => 'http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key'
+      },
+      include     => {
+        deb => true,
+        src => false
+      }
     }
   }
   else {
@@ -24,9 +29,14 @@ class jenkins::repo::debian
       location    => 'http://pkg.jenkins-ci.org/debian',
       release     => 'binary/',
       repos       => '',
-      key         => '150FDE3F7787E7D11EF4E12A9B7D32F2D50582E6',
-      key_source  => 'http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key',
-      include_src => false,
+      key         => {
+        id      => '150FDE3F7787E7D11EF4E12A9B7D32F2D50582E6',
+        source  => 'http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key'
+      },
+      include     => {
+        deb => true,
+        src => false
+      }
     }
   }
 
